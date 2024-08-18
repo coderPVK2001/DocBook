1e845c833881c64efbfd7ca730d9434e82877cbf
Prajwal <prajwalvkande2001@gmail.com>
2024-08-18 23:48:58 +0530

    city added

---
+++ src/main/java/com/docbook/area/entity/Area.java
@@ -0,0 +1,19 @@
+package com.docbook.area.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "area")
+public class Area {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String areaname;
+
+}
