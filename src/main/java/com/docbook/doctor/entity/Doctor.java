6769364280f3a808840d43842b546bb964025235
Prajwal <prajwalvkande2001@gmail.com>
2024-08-19 23:14:03 +0530

    doctor entity & repository created

---
+++ src/main/java/com/docbook/doctor/entity/Doctor.java
@@ -0,0 +1,49 @@
+package com.docbook.doctor.entity;
+
+import com.docbook.area.entity.Area;
+import com.docbook.city.entity.City;
+import com.docbook.clinic.entity.Clinic;
+import com.docbook.specializations.entity.Specialization;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "doctor")
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "doctor_name", nullable = false, length = 100)
+    private String doctorName;
+
+    @Column(name = "mobile", nullable = false, unique = true, length = 15)
+    private String mobile;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "image_url", length = 500)
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "area_id")
+    private Area area;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @ManyToOne
+    @JoinColumn(name = "specialization_id")
+    private Specialization specialization;
+
+    @ManyToOne
+    @JoinColumn(name = "clinic_id")
+    private Clinic clinic;
+
+}
---
+++ src/main/java/com/docbook/doctor/repository/DoctorRepository.java
@@ -0,0 +1,7 @@
+package com.docbook.doctor.repository;
+
+import com.docbook.doctor.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+}
